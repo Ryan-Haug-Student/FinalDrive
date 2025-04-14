6c0d21a9411dc58bc4a6845e2cdcdc06b2d22f60
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-04-14 14:21:24 -0400

    small tweak to road lines

--- Assets/Scripts/DecortaionObj.cs
+++ Assets/Scripts/DecortaionObj.cs
@@ -7,8 +7,6 @@ public class DecortaionObj : MonoBehaviour
     private int speed = 25; //link to Player controller speed when made
     private float DFZ = -1; //distance from zero, used for paralax
 
-    private int maxZ = 300;
-
     private void Start()
     {   //adding and dividing DFZ by 100 to even out the distances, creating a smoother paralax
         DFZ = Mathf.Abs(transform.position.x) + 101;
@@ -16,7 +14,7 @@ public class DecortaionObj : MonoBehaviour
 
     private void Update()
     {
-        if (transform.position.x < -maxZ)
+        if (transform.position.z < -150)
             Destroy(gameObject);
 
         transform.position += Vector3.back * speed / (DFZ / 100) * Time.deltaTime;
