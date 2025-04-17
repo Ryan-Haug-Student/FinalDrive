0ad1065ba8d6fe1c6add34237493b645ad445d12
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-04-17 14:16:39 -0400

    STEERING AND MOVEMENT WORKS WOOOOOO

---
+++ Assets/Scripts/Bike.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike : MonoBehaviour
+{
+    public float lean;
+    public GameObject pivot;
+    public HingeJoint joint;
+
+
+    private void Update()
+    {
+        Lean();
+        Move();
+
+        transform.localPosition = new Vector3(0, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    private void Lean()
+    {
+        lean = joint.angle;
+
+        pivot.transform.rotation = Quaternion.Euler(0, 0, -lean * .8f);
+    }
+
+    private void Move()
+    {
+        if (-lean < 0 && pivot.transform.position.x < 5.6f)
+            pivot.transform.position += new Vector3(0.001f * lean, 0, 0);
+        else if (-lean > 0 && pivot.transform.position.x > -5.6f)
+            pivot.transform.position += new Vector3(0.001f * lean, 0, 0);
+    }
+}
