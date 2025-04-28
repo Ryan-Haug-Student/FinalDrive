c75b2062d2425bb4c28bc9a056ce4fce82aac86a
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-04-28 12:54:44 -0400

    now displaying the score multiplier

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public TMP_Text multText;
+
     public void Traffic(int num)
     {
         PlayerPrefs.SetInt("carFreq", num);
@@ -32,5 +35,21 @@ public class ButtonManager : MonoBehaviour
     {
         PlayerPrefs.Save();
         print("Updated!");
+    }
+
+    private void Update()
+    {
+        float multiplier;
+        int lives;
+
+        multiplier = 2.2f - (PlayerPrefs.GetInt("lanes") * .2f);
+        multiplier -= PlayerPrefs.GetInt("carFreq") * .2f;
+
+        lives = PlayerPrefs.GetInt("lives");
+
+        if (lives != 1)
+            multiplier /= 2;
+
+        multText.text = $"Score Multiplier: {Mathf.Round(multiplier * 100) / 100}";
     }
 }
